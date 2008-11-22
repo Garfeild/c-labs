85f12ac3e4f2b960111cf69cd421464d7eb00e2d
Garfeild <garfeild.ubuntu@gmail.com>
2008-11-23 02:59:12 +0300

    laba_03 ready

--- laba_03.c
+++ laba_03.c
@@ -21,43 +21,55 @@
 int main (int argc, char *argv[])
 {
   int pid1, pid2, st, fd;
-  int temp = 0;
   int n = 0;
   char ch = '*';
   char ch2 = '!';
   struct stat filest;
+  struct stat temp2;
   
   if ( argc == 3 )  {
     printf("Process 1, PID %d\n", pid1 = getpid());
     n = (int)atoi(argv[2]);
-    printf("%d\n", n);
-    if ( (fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, PERMS)) != -1 )  {
-      
-      fstat(fd, &filest);
-      temp = filest.st_atime;
-      printf("%d\n", temp);
-      if ( (pid2 = fork()) == 0 ) {
-        printf("Process 2, PID %d\n", pid2 = getpid());
-        while( temp == filest.st_atime ) {
-          printf("p2:%d\n", (int)filest.st_atime);
-          fstat(fd, &filest);
-        }
-        
-        while( n != 0)  {
-          n--;
-          write(fd, &ch2, 1);
-        }
+    if ( (fd = open(argv[1],  O_RDWR )) == -1 ) {
+      if ( (fd = open(argv[1], O_CREAT| O_RDONLY, PERMS)) == -1)  {
+        printf("Error: can't open file %s\n", argv[1]);
+        exit(-1);
       }
       else  {
-        while( n != 0)  {
-          n--;
-          write(fd, &ch, 1);
-        }
-        read(fd, &ch, 1);
+        sleep(1);
         close(fd);
       }
-      wait(&st);
     }
+    fd = open(argv[1], O_RDWR | O_TRUNC);
+    fstat(fd, &filest);
+    temp2 = filest;
+    
+    if ( (pid2 = fork()) == 0 ) {
+      printf("Process 2, PID %d\n", pid2 = getpid());
+      while( (int)temp2.st_mtime == (int)filest.st_mtime && (int)temp2.st_atime == (int)filest.st_atime )  {
+        fstat(fd, &filest);
+        }
+      
+      printf("Process 2 is writing\n");
+      while( n != 0)  {
+        n--;
+        write(fd, &ch2, 1);
+      }
+    }
+    else  {
+      printf("Process 1 is writing\n");
+      while( n != 0)  {
+        n--;
+        write(fd, &ch, 1);
+      }
+      read(fd, &ch, 1);
+      close(fd);
+    }
+    wait(&st);
+  } 
+  else  {
+    printf("Error!\n\tUsage: forki /path/to/file <number of bytes>\n");
+    exit(-1);
   }
 
   return 0;
